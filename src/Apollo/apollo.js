ccbe946edbc49c686d1675ad1a23043a520fc923
SeoJun Park <jinchul112@gmail.com>
2021-02-27 19:29:18 +0100

    update

--- src/Apollo/apollo.js
+++ src/Apollo/apollo.js
@@ -1,18 +1,18 @@
 import {
-  ApolloClient,
-  ApolloLink,
-  concat,
-  gql,
-  HttpLink,
-  InMemoryCache,
-  makeVar,
-  split,
+    ApolloClient,
+    ApolloLink,
+    concat,
+    gql,
+    HttpLink,
+    InMemoryCache,
+    makeVar,
+    split,
 } from "@apollo/client";
 import { WebSocketLink } from "@apollo/client/link/ws";
 import { onError } from "@apollo/client/link/error";
 import { getMainDefinition } from "@apollo/client/utilities";
 
-const typeDefs = gql`
+const typeDefs = gql `
   extend type Query {
     isLoggedIn: Boolean!
   }
@@ -21,67 +21,67 @@ const typeDefs = gql`
 export const isLoggedInVar = makeVar(!!localStorage.getItem("Bearer"));
 
 const cache = new InMemoryCache({
-  typePolicies: {
-    Query: {
-      fields: {
-        isLoggedIn: {
-          read() {
-            return isLoggedInVar();
-          },
+    typePolicies: {
+        Query: {
+            fields: {
+                isLoggedIn: {
+                    read() {
+                        return isLoggedInVar();
+                    },
+                },
+            },
         },
-      },
     },
-  },
 });
 
 const authMiddle = new ApolloLink((operation, forward) => {
-  operation.setContext({
-    headers: {
-      Authorization: localStorage.getItem("Bearer") || "",
-    },
-  });
-  return forward(operation);
+    operation.setContext({
+        headers: {
+            Authorization: localStorage.getItem("Bearer") || "",
+        },
+    });
+    return forward(operation);
 });
 
 const wsLink = new WebSocketLink({
-  uri: `ws://localhost:4000/subscription`,
-  options: {
-    reconnect: true,
-    lazy: true,
-    connectionParams: {
-      Bearer: localStorage.getItem("Bearer") || "",
+    uri: `ws://localhost:4000/subscription`,
+    options: {
+        reconnect: true,
+        lazy: true,
+        connectionParams: {
+            Bearer: localStorage.getItem("Bearer") || "",
+        },
     },
-  },
 });
 
 const httpLink = new HttpLink({
-  uri: `http://localhost:4000/graphql`,
+    uri: `http://localhost:4000/graphql`,
 });
 
 const errLink = onError(({ graphQLErrors, networkError }) => {
-  if (graphQLErrors) {
-    graphQLErrors.map(({ message }) =>
-      console.log("network graphql error: ", message)
-    );
-  }
+    if (graphQLErrors) {
+        graphQLErrors.map(({ message }) =>
+            console.log("network graphql error: ", message)
+        );
+    }
 });
 
 const linkCombine = split(
-  ({ query }) => {
-    const { kind } = getMainDefinition(query);
-    return kind === "OperationDefinition";
-  },
-  wsLink,
-  httpLink
+    ({ query }) => {
+        const { kind, operation } = getMainDefinition(query);
+        return kind === "OperationDefinition" && operation === "subscription";
+    },
+    wsLink,
+    httpLink
 );
 
 const client = new ApolloClient({
-  cache,
-  link: ApolloLink.from([errLink, concat(authMiddle, linkCombine)]),
-  headers: {
-    authorization: localStorage.getItem("Bearer") || "",
-  },
-  typeDefs,
+    cache,
+    link: ApolloLink.from([errLink, concat(authMiddle, linkCombine)]),
+    headers: {
+        authorization: localStorage.getItem("Bearer") || "",
+    },
+    typeDefs,
 });
 
 export default client;
